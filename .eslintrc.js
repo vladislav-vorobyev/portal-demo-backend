381c2bac66e61b66b85d6188b469d5d27ecd0742
Vladislav Vorobyev <Vladislav.Vorobyov@soft-werke.com>
2023-03-02 21:38:07 +0300

    project init

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  overrides: [],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: { "no-console": 0 },
+};
